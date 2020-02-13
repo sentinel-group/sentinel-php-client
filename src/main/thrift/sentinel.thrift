cd610a383bddee4ea7e22d4df18f258bdba0c9ad
hanyong <hanyong2017@qq.com>
2020-02-13 13:55:41 +0800

    SentinelSidecar 启动 thrift 服务

--- src/main/thrift/sentinel.thrift
+++ src/main/thrift/sentinel.thrift
@@ -1,9 +1,17 @@
 namespace java com.alibaba.csp.sentinel.sidecar.rpc
 namespace php Sentinel.Rpc
 
+exception BlockException {
+
+	1:string ruleLimitApp;
+	
+	2:string message;
+	
+}
+
 service Sentinel {
 
-	i32 entry(1:string name);
+	i32 entry(1:string name) throws ( 1:BlockException ex );
 	
 	void exit(1:i32 id);
 
