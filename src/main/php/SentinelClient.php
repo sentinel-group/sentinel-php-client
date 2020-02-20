678a80b57c0d6c1c2bd684c15ccb202ec2ad7575
hanyong <hanyong2017@qq.com>
2020-02-20 22:25:48 +0800

    php SentinelEntry 实现资源释放处理

--- src/main/php/SentinelClient.php
+++ src/main/php/SentinelClient.php
@@ -3,16 +3,34 @@
 namespace Sentinel;
 
 use Sentinel\Rpc\SentinelRpcClient;
+use Thrift\Exception\TException;
+use Thrift\Exception\TTransportException;
 use Thrift\Protocol\TBinaryProtocolAccelerated;
 use Thrift\Transport\TFramedTransport;
 use Thrift\Transport\TSocket;
 
+/**
+ * Sentinel 客户端。通过 RPC 调用 sidecar 实现。
+ * TODO 多线程时底层 RPC 通信必须加锁串行访问
+ *
+ * @package Sentinel
+ */
 class SentinelClient
 {
-    protected $client = null;
+    /**
+     * 底层 RPC 通信 socket 。
+     *
+     * @var TSocket
+     */
+    protected $socket_ = null;
 
     /**
-     * Client constructor.
+     * @var SentinelRpcClient
+     */
+    protected $client_ = null;
+
+    /**
+     * SentinelClient constructor.
      * TODO 是否可使用 persistent socket 优化性能 ?
      *
      * @param string $host Sentinel sidecar socket hostname
@@ -25,19 +43,41 @@ class SentinelClient
         $persist = false
     )
     {
-        $socket = new TSocket($host, $port, $persist);
-        if (!$socket->isOpen()) {
-            $socket->open();
-        }
+        $this->socket_ = new TSocket($host, $port, $persist);
 
-        $trans = new TFramedTransport($socket);
+        $trans = new TFramedTransport($this->socket_);
         $protocol = new TBinaryProtocolAccelerated($trans, true, true);
         $input = $protocol;
         $output = $protocol;
-        $this->client = new SentinelRpcClient($input, $output);
+        $this->client_ = new SentinelRpcClient($input, $output);
     }
 
+    /**
+     * 确认打开底层 socket 连接。
+     *
+     * @throws TTransportException
+     * @throws TException
+     */
+    protected function ensureOpen() {
+        if (!$this->socket_->isOpen()) {
+            $this->socket_->open();
+        }
+    }
+
+    /**
+     * 获取受保护的资源访问入口。
+     * 返回对象不再被引用后, 将自动释放访问入口。
+     *
+     * 注意: 必须定义一个变量持有访问入口, 否则返回对象被自动销毁, 将自动释放访问入口。
+     *
+     * @param string $name 资源名称
+     * @return SentinelEntry
+     * @throws BlockException
+     * @throws TException
+     */
     public function entry($name) {
-        $id = $this->client->entry($name);
+        $this->ensureOpen();
+        $id = $this->client_->entry($name);
+        return new SentinelEntry($this->client_, $id);
     }
 }
--- src/main/php/SentinelEntry.php
+++ src/main/php/SentinelEntry.php
@@ -2,7 +2,43 @@
 
 namespace Sentinel;
 
+use Sentinel\Rpc\SentinelRpcClient;
+
+/**
+ * 受保护的资源访问入口。
+ *
+ * 调用 SentinelClient.entry() 成功后返回此类对象，
+ * 其持有受保护的资源访问入口，销毁此类对象即自动释放资源访问入口。
+ *
+ * @package Sentinel
+ */
 class SentinelEntry
 {
+    /**
+     * @var SentinelRpcClient
+     */
+    protected $client_ = null;
 
+    /**
+     * @var int
+     */
+    protected $id_ = null;
+
+    /**
+     * SentinelEntry constructor.
+     * 调用 SentinelClient.entry() 成功后返回此类对象。
+     *
+     * @param SentinelRpcClient $client
+     * @param int $id
+     */
+    public function __construct($client, $id)
+    {
+        $this->client_ = $client;
+        $this->id_ = $id;
+    }
+
+    public function __destruct()
+    {
+        $this->client_->exit($this->id_);
+    }
 }
