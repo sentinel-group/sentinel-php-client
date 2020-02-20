2cfe0283af06420099b82d7ef8e0a41a92fe2dad
hanyong <hanyong2017@qq.com>
2020-02-20 16:25:32 +0800

    sentinel.thrift update

--- src/main/thrift/sentinel.thrift
+++ src/main/thrift/sentinel.thrift
@@ -1,5 +1,7 @@
+// vim: set noet ts=4 sw=4:
+
 namespace java com.alibaba.csp.sentinel.sidecar.rpc
-namespace php Sentinel.Rpc
+namespace php  Sentinel.Rpc
 
 exception BlockException {
 
@@ -12,7 +14,8 @@ exception BlockException {
 service Sentinel {
 
 	i32 entry(1:string name) throws ( 1:BlockException ex );
-	
+
+	// TODO "exit" 在 php 中是关键字, php 7.0 才支持使用此作为自定义名字. 要支持 php 5.5 则需要改名 ?
 	void exit(1:i32 id);
 
 }
